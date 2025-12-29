90b6d202af0982eb77a13a6a36d8d2403084925e
Anagoat <39483091+anagoat@users.noreply.github.com>
2025-12-29 14:46:16 +0200

    Remove reply_markup from photo request form
    Removed the reply_markup from the form data before sending the photo request.

--- usercode.js
+++ usercode.js
@@ -3,7 +3,7 @@ module.exports = (data) => {
   const TOKEN = data.token;
   const chatId = data.chat_id; // або звідки ти береш chat_id
 
-  const binary = atob(data.base64Qr || base64Qr);
+  const binary = atob(data.base64Qr);
   const bytes = new Uint8Array(binary.length);
   for (let i = 0; i < binary.length; i++) {
     bytes[i] = binary.charCodeAt(i);
@@ -15,7 +15,7 @@ module.exports = (data) => {
   form.append("photo", blob, "qr.png");
   form.append("caption", "Обери дію");
   form.append("parse_mode", "Markdown");
-  form.append("reply_markup", JSON.stringify(replyMarkup));
+  // form.append("reply_markup", JSON.stringify(replyMarkup));
 
   data.photo_request_data = {
     method: "POST",
